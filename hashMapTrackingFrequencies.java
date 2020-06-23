85e7b64a08da6892a1f93786996f0ab0e18bdad4
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-06-23 15:19:40 -0400

    Update hashMapTrackingFrequencies.java

--- hashMapTrackingFrequencies.java
+++ hashMapTrackingFrequencies.java
@@ -1,20 +1,20 @@
 import java.util.HashMap;
+
 class Main {
   public static void main(String[] args) {
-    int[]arr = new int[] {10, 20, 20, 10, 10, 30, 50, 10, 20};
-    HashMap<Integer, Integer> map = new HashMap<>();        
-        for (int number : arr) {
-            if (map.containsKey(number)) {
-              Integer count = map.get(number);
-                if (count == null) {
-			              map.put(number, 1);
-                }
-                else {
-                  map.put(number, count + 1);
-                }
-            } else             
-                map.put(number,1);
+    int[] arr = new int[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for (int number : arr) {
+      Integer count = map.get(number);
+      if (map.containsKey(number)) {
+        if (count == null) {
+          map.put(number, 1);
+        } else {
+          map.put(number, count + 1);
         }
-        System.out.println(map); //{50=1, 20=3, 10=4, 30=1}
+      } else
+        map.put(number, 1);
+    }
+    System.out.println(map);
   }
 }
