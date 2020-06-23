b9ba6deef47d51f68f5cf1f1696fb2f36df8d8c7
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-06-23 13:51:50 -0400

    Update hashMapTrackingPairs.java

--- hashMapTrackingPairs.java
+++ hashMapTrackingPairs.java
@@ -1,16 +1,16 @@
 import java.util.HashMap;
 class Main {
   public static void main(String[] args) {
-    int[]ar = new int[] {10, 20, 20, 10, 10, 30, 50, 10, 20};
+    int[]arr = new int[] {10, 20, 20, 10, 10, 30, 50, 10, 20};
     HashMap<Integer, Integer> map = new HashMap<>(); 
-        int pairsOfSocks = 0;
-        for (int number : ar) {
+        int pairs = 0;
+        for (int number : arr) {
             if (map.containsKey(number)) {
                 map.remove(number);
-                pairsOfSocks++;
+                pairs++;
             } else             
                 map.put(number,1);
         }
-        System.out.println(pairsOfSocks);
+        System.out.println(pairs);
   }
 }
